1aefe1dde9bf83735d0bfd349fe54bdc9d7ab380
Max Nankivell <maxnankivell1@gmail.com>
2026-03-21 21:10:43 +1300

    begin adding data to add data page

--- src/app/add-data/page.tsx
+++ src/app/add-data/page.tsx
@@ -6,9 +6,16 @@ export const metadata: Metadata = {
 
 export default function AddData() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-      <h1>Add Data</h1>
-      <p>This is a add data page.</p>
+    <div className="flex flex-1 flex-col w-full items-center sm:items-start text-center sm:text-left">
+      <h1
+        className={`max-w-3xl text-5xl font-bold leading-12 tracking-tight mb-4 text-heading`}
+      >
+        Add Your Data
+      </h1>
+      <p className={`max-w-3xl text-lg leading-8 mb-8 text-subheading`}>
+        Enter your data and then choose what kind of ranking to make and how to
+        make it.
+      </p>
     </div>
   );
 }
